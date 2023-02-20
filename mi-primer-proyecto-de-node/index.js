d12f7dbe3d49a449f310672fc2bd4c1171000081
GersonRG23 <antonyrgs00@gmail.com>
2023-02-20 16:33:53 -0500

    Se agrega el folder de Javascript

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,8 @@
+//Este es el mensaje de bienvnenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+//Esta es la parte intermedia de nuestro proyecto
+console.log("Deje la maricada viejo gonorrea...hijueputa");
+
+//Este es el mensaje que se envia para despedirse
+console.log("Hasta luego perro jijijijijepeuta");
